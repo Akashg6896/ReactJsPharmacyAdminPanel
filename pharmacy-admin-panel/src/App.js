95ddbff03c416cd3c4cf0becfabf310bc5aaa080
Akash <amakash6896@gmail.com>
2022-08-15 00:23:36 +0530

    Updated commit

--- pharmacy-admin-panel/src/App.js
+++ pharmacy-admin-panel/src/App.js
@@ -84,7 +84,7 @@ function App() {
           }
         />
         <Route
-          path='*'
+          path='/ReactJsPharmacyAdminPanel'
           element={
             !isLoggedIn ? <Login userHandler={userHandler} /> : <Orders />
           }
